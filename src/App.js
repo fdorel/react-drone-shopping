642bb6ba246d83df477e01ab62a753a3789ce92a
Dorel Furdui <dorelfurdui@gmail.com>
2026-02-09 22:16:08 -0500

    Fix import statement formatting in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import './App.css'
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
